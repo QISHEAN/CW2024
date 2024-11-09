b74ded5ea473d3f9ef0b37c44301f064fe9c80d8
qisheanoh0814@gmail.com <your.email@example.com>
2024-11-09 13:02:19 +0800

    remove observer at the current level before going to the next level , this is to prevent memory leaks.

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -15,6 +15,7 @@ public class Controller implements Observer {
 
 	private static final String LEVEL_ONE_CLASS_NAME = "com.example.demo.LevelOne";
 	private final Stage stage;
+	private LevelParent currentLevel;  // Reference to the current level
 
 	public Controller(Stage stage) {
 		this.stage = stage;
@@ -23,20 +24,29 @@ public class Controller implements Observer {
 	public void launchGame() throws ClassNotFoundException, NoSuchMethodException, SecurityException,
 			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException  {
 
-			stage.show();
-			goToLevel(LEVEL_ONE_CLASS_NAME);
+		stage.show();
+		goToLevel(LEVEL_ONE_CLASS_NAME);
 	}
 
 	private void goToLevel(String className) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
 			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-			Class<?> myClass = Class.forName(className);
-			Constructor<?> constructor = myClass.getConstructor(double.class, double.class);
-			LevelParent myLevel = (LevelParent) constructor.newInstance(stage.getHeight(), stage.getWidth());
-			myLevel.addObserver(this);
-			Scene scene = myLevel.initializeScene();
-			stage.setScene(scene);
-			myLevel.startGame();
-
+		deleteObservableOnLevelChange();
+		Class<?> myClass = Class.forName(className);
+		Constructor<?> constructor = myClass.getConstructor(double.class, double.class);
+		LevelParent myLevel = (LevelParent) constructor.newInstance(stage.getHeight(), stage.getWidth());
+		currentLevel = myLevel;
+		myLevel.addObserver(this);
+		Scene scene = myLevel.initializeScene();
+		stage.setScene(scene);
+		myLevel.startGame();
+	}
+	private void deleteObservableOnLevelChange() {
+		if (currentLevel != null) {
+			currentLevel.deleteObserver(this); // Remove this controller as an observer
+			stage.setScene(null);  // Remove the current scene
+			// Nullify currentLevel to free up resources
+			currentLevel = null;
+		}
 	}
 
 	@Override
@@ -44,7 +54,7 @@ public class Controller implements Observer {
 		try {
 			goToLevel((String) arg1);
 		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException
-				| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+				 | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setContentText(e.getClass().toString());
 			alert.show();
