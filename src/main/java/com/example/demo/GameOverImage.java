3969e6a03c1053bf61941d16acbbfe6bc37c76d1
qisheanoh0814@gmail.com <your.email@example.com>
2024-11-09 12:45:54 +0800

    centralise the image of game over

--- src/main/java/com/example/demo/GameOverImage.java
+++ src/main/java/com/example/demo/GameOverImage.java
@@ -3,15 +3,25 @@ package com.example.demo;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class GameOverImage extends ImageView {
-	
-	private static final String IMAGE_NAME = "/com/example/demo/images/gameover.png";
+import java.util.Objects;
 
+
+public class GameOverImage extends ImageView {
+
+	private static final String IMAGE_NAME = "/com/example/demo/images/gameover.png";
+	private static final double DEFAULT_WIDTH = 500;
 	public GameOverImage(double xPosition, double yPosition) {
-		setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()) );
+		setImage(new Image(Objects.requireNonNull(getClass().getResource(IMAGE_NAME)).toExternalForm()) );
 //		setImage(ImageSetUp.getImageList().get(ImageSetUp.getGameOver()));
 		setLayoutX(xPosition);
 		setLayoutY(yPosition);
+
+		setPreserveRatio(true);
+
+		// Set a default fit width (or height) to resize the image
+		setFitWidth(DEFAULT_WIDTH); // Adjust width as necessary
+
+
 	}
 
 }
