ba496233412ef77259aa2de186c5681de5caa4d2
qisheanoh0814@gmail.com <your.email@example.com>
2024-11-10 15:08:08 +0800

    export main application package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo.controller;
+    opens com.example.demo.controller to javafx.fxml;  // Open to javafx.fxml for FXML access
+    exports com.example.demo;  // Export your main application package
+    exports com.example.demo.controller;  // Add this line to export the controller package to javafx.graphics
 }
