ab8a87c05f6d58d11bb0420e366c8e0103897c31
qisheanoh0814@gmail.com <your.email@example.com>
2024-11-25 20:23:22 +0800

    import java.util.Objects to simplify common operations and improve readability. requireNotNull() ensure early and explicit null checks,helping you catch errors.

--- src/main/java/com/example/demo/ui/ShieldImage.java
+++ src/main/java/com/example/demo/ui/ShieldImage.java
@@ -3,15 +3,17 @@ package com.example.demo.ui;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.util.Objects;
+
 public class ShieldImage extends ImageView {
 
 	private static final int SHIELD_SIZE = 150;
 
-	public ShieldImage(double xPosition, double yPosition) {
+	public ShieldImage() {
 		this.setLayoutX(1150);
 		this.setLayoutY(5);
 		//this.setImage(new Image(IMAGE_NAME));
-		this.setImage(new Image(getClass().getResource("/com/example/demo/images/shield.png").toExternalForm()));
+		this.setImage(new Image(Objects.requireNonNull(getClass().getResource("/com/example/demo/images/shield.png")).toExternalForm()));
 		this.setVisible(false);
 		this.setFitHeight(SHIELD_SIZE);
 		this.setFitWidth(SHIELD_SIZE);
