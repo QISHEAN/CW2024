b6dd98bb5354f08327d01931bd847adf9e1cdff0
qisheanoh0814@gmail.com <your.email@example.com>
2024-11-25 16:53:13 +0800

    packages level, levelview, actor, projectiles,ui are exported

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,10 @@ module com.example.demo {
     requires javafx.fxml;
 
     opens com.example.demo.controller to javafx.fxml;  // Open to javafx.fxml for FXML access
-    exports com.example.demo;  // Export your main application package
-    exports com.example.demo.controller;  // Add this line to export the controller package to javafx.graphics
+    exports com.example.demo.controller;
+    exports com.example.demo.level;
+    exports com.example.demo.levelview;
+    exports com.example.demo.actor;
+    exports com.example.demo.projectiles;
+    exports com.example.demo.ui;  // Add this line to export the controller package to javafx.graphics
 }
