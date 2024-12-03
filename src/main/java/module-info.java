8db27313c9b3cabd84dc76b09c71d976842aa3f4
qisheanoh0814@gmail.com <your.email@example.com>
2024-12-03 17:06:58 +0800

    The javafx.media module has been added.This allows the application to use JavaFX's media-related features, such as playing audio and video, which are essential for functionalities provided by the SoundManager class.
    he com.example.demo.managers package has been exported.This ensures that other modules or parts of the application can access classes in the managers package, such as SoundManager.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.logging;
+    requires javafx.media;
 
     opens com.example.demo.controller to javafx.fxml;  // Open to javafx.fxml for FXML access
     exports com.example.demo.controller;
@@ -9,5 +10,6 @@ module com.example.demo {
     exports com.example.demo.levelview;
     exports com.example.demo.actor;
     exports com.example.demo.projectiles;
+    exports com.example.demo.managers;
     exports com.example.demo.ui;  // Add this line to export the controller package to javafx.graphics
 }
